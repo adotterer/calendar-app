70b0954da00d776c9d9d46791daa021fd0d4dead
adotterer <adotterer@gmail.com>
2024-09-01 17:23:22 -0500

    Row creation successful

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import MonthView from "../components/Calendar/MonthView";
+import { supabase } from "@/lib/supabase";
 
 export default function Home() {
+  const setNewView = async () => {
+    const { data, error } = await supabase.from("views").insert({
+      name: "random name",
+    });
+    if (data) console.log(data);
+    if (error) console.log(error);
+  };
+
+  setNewView();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MonthView />
