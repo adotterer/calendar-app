1d62a8b13472e4e51a29f93307c80e155b9240b3
adotterer <adotterer@gmail.com>
2024-09-02 14:37:10 -0500

    Middleware auth login code

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 import MonthView from "../components/Calendar/MonthView";
-import { supabase } from "@/lib/supabase";
+// import { supabaseAdmin } from "@/lib/supabase";
+import Login from "@/components/login/page";
 
 export default function Home() {
-  const setNewView = async () => {
-    const { data, error } = await supabase.from("views").insert({
+  /*   const setNewView = async () => {
+    const { data, error } = await supabaseAdmin.from("views").insert({
       name: "random name",
     });
     if (data) console.log(data);
     if (error) console.log(error);
-  };
+  }; */
 
-  setNewView();
+  // setNewView();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Login />
       <MonthView />
     </main>
   );
