5545fd1268430bd6aacf5226730b9c330138fe2f
adotterer <adotterer@gmail.com>
2024-10-13 23:47:29 -0400

    Rm unused

--- components/AuthForm/button.tsx
+++ components/AuthForm/button.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import { useAuth } from "@/context/AuthContext";
 import AuthForm from ".";
